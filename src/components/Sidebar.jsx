3da4fd420d73291c2fb944db6e59a7dc1be27d40
laura <mbernallaura@gmail.com>
2024-01-19 00:34:56 -0500

    Hacer la seleccion en el sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,9 +6,11 @@ import { FaArrowCircleLeft, FaList, FaHome } from "react-icons/fa";
 const Sidebar = () => {
     const [open, setOpen] = useState(false);
     const [id, setId] = useState();
+    const [selectedOption, setSelectedOption] = useState("Home"); // Estado para la opción seleccionada
+
     const sidebar = [
-        {name: 'Home', src: <FaHome  fontSize={30}/>, link: `/login/${id}`},
-        {name: 'List', src: <FaList fontSize={30}/>, link:`/login/${id}/list`}
+        { name: 'Home', src: <FaHome fontSize={30} />, link: `/login/${id}` },
+        { name: 'List', src: <FaList fontSize={30} />, link: `/login/${id}/list` }
     ];
 
     useEffect(() => {
@@ -19,19 +21,22 @@ const Sidebar = () => {
         <main className={`${open ? "w-60" : "w-28"} h-screen pt-24 relative font-openSans duration-300 bg-blueLight`}>
             <FaArrowCircleLeft fontSize={35} onClick={()=> setOpen(!open)} className={`${!open && "rotate-180"} -right-4 top-10 absolute cursor-pointer text-grey bg-white rounded-full`}/>
             <ul>
-                {
-                    sidebar.map((sidebar, index)=>(
-                        <Link key={index} href={sidebar.link}>
-                            <li className=" p-10 left-10 flex items-center text-center text-2xl gap-x-4 cursor-pointer font-semibold text-white hover:bg-blueDarksito">
-                                {sidebar.src}
-                                <span className={`${!open && "hidden origin-left duration-200"}`}>{sidebar.name}</span>
-                            </li>
-                        </Link>
-                    ))
-                }
+                {sidebar.map((sidebarItem, index) => (
+                    <Link key={index} href={sidebarItem.link}>
+                        <li
+                            className={`p-10 left-10 flex items-center text-center text-2xl gap-x-4 cursor-pointer font-semibold text-white hover:bg-blueDarksito ${
+                                selectedOption === sidebarItem.name && "bg-blueDarksito"
+                            }`}
+                            onClick={() => setSelectedOption(sidebarItem.name)} 
+                        >
+                            {sidebarItem.src}
+                            <span className={`${!open && "hidden origin-left duration-200"}`}>{sidebarItem.name}</span>
+                        </li>
+                    </Link>
+                ))}
             </ul>
         </main>
-    )
-}
+    );
+};
 
 export default Sidebar

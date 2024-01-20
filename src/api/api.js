4fe05db53f8efb6252ee1036c330714816a35422
laura <mbernallaura@gmail.com>
2024-01-19 21:49:15 -0500

    Funcionalidad de la parte document en la pagina new sale

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,60 @@
 export let norteDigital = {
     id: 1,
     username: 'roberto',
-    password: '1234'
+    password: '1234',
+    costomers:[
+        {
+            id: 1,
+            name: 'Diego Martinez',
+            branches:[
+                {
+                    id:1,
+                    nameBranch: 'Colombia',
+                    currency: 'COP',
+                    products:[
+                        {
+                            id: 1,
+                            name: 'Cuaderno',
+                            price: 1500,
+                        },
+                        {
+                            id: 2,
+                            name: 'Regla',
+                            price: 3000
+                        }
+                    ],
+                    sales:[]
+                }
+            ],
+        },
+        {
+            id: 2,
+            name: 'Camila Suarez',
+            branches:[
+                {
+                    id:1,
+                    nameBranch: 'Argentina',
+                    currency: 'ARS',
+                    products:[
+                        {
+                            id: 1,
+                            name: 'Blusa',
+                            price: 15000,
+                        },
+                        {
+                            id: 2,
+                            name: 'Jean',
+                            price: 19000
+                        },
+                        {
+                            id: 2,
+                            name: 'Medias',
+                            price: 1500
+                        }
+                    ],
+                    sales:[]
+                }
+            ],
+        }
+    ]
 };
--- src/components/FormNewSale.jsx
+++ src/components/FormNewSale.jsx
@@ -2,15 +2,29 @@
 import { useState } from "react";
 import Details from "./Details";
 import Title from "./Title";
+import { norteDigital } from "@/api/api";
+import ReactSelect from "react-select";
+import Select from "react-select";
+// import Select from "react-select/dist/declarations/src/Select";
 
 const FormNewSale = () => {
     const [arrayDetails, setArrayDetails] = useState([<Details/>]);
-    // const [arrayDetails, setArrayDetails] = useState([<Details key={0} />]);
+    const [arrayBranch, setArrayBranch] = useState([]);
 
     const addDetails = () =>{
-        setArrayDetails([...arrayDetails, <Details/>]);
-        // setArrayDetails(prevDetails => [...prevDetails, <Details key={prevDetails.length} />]);
+        setArrayDetails([...arrayDetails, <Details/>]);;
     }
+
+    const handleSelectClient = ({value}) =>{
+        const customer = norteDigital.costomers.find(data => data.id === value);
+        setArrayBranch(customer.branches);
+    }
+
+    const handleSelectBranch = () =>{
+        const currency = arrayBranch.map(data =>(data.currency));
+        document.getElementById('currency').value = currency;
+    }
+    
 
     return (
         <main className="flex flex-col pt-10">
@@ -20,19 +34,28 @@ const FormNewSale = () => {
                     <div className="w-[calc(40%)] flex flex-col">
                         <label htmlFor="" className="font-semibold text-grey">Client</label>
                         <div className="flex gap-4">
-                            <input type="text" className="w-full"/>
+                            <Select 
+                                className="w-full"
+                                options={norteDigital.costomers.map(data => ({label: data.name, value: data.id}))}
+                                onChange={handleSelectClient}
+                            />
+                            {/* <input type="text" className="w-full"/> */}
                             <button className="p-2 px-4 font-extrabold text-white bg-blueLight">+</button>
                         </div>
                     </div>
                     
                     <div className="w-[calc(50%)] flex flex-col">
                         <label htmlFor="" className="font-semibold text-grey">Branch Office</label>
-                        <input type="text" className="p-2 w-full"/>
+                        <Select
+                            className="w-full"
+                            options={arrayBranch.map(data => ({label: data.nameBranch, value: data.id}))}
+                            onChange={handleSelectBranch}
+                        />
                     </div>
 
                     <div className="w-[calc(10%)] flex flex-col">
                         <label htmlFor="" className="font-semibold text-grey">Currency</label>
-                        <input type="text" className="p-2 w-full"/>
+                        <input type="text" name="currency" id="currency" className="p-2 w-full" readOnly/>
                     </div>
                 </div>
 
